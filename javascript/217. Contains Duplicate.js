9076beaf93ca3419f069e58ef2f5a96cda90fd1c
Ruslan Fatykhov <rufatykhov@gmail.com>
2022-08-09 17:30:20 +0300

    217. Contains Duplicate

--- javascript/217. Contains Duplicate.js
+++ javascript/217. Contains Duplicate.js
@@ -1,5 +1,5 @@
 const containsDuplicate = (nums) => {
-	const unique = Array.from(new Set(nums));
+	const unique = [...new Set(nums)];
 	if (nums.length === unique.length) {
 		return false;
 	} else {
