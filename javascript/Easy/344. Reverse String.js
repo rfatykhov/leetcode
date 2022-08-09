2ce2a18d1b43a73f7d6b545df6ad84220ac1b1a0
Ruslan Fatykhov <rufatykhov@gmail.com>
2022-08-09 17:54:10 +0300

    344. Reverse String

---
+++ javascript/Easy/344. Reverse String.js
@@ -0,0 +1,24 @@
+//Solution 1 (while loop)
+
+const reverseString = (s) => {
+    let start = 0;
+    let end = s.length - 1;
+
+    while (start <= end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+};
+
+//Solution 2 (for loop)
+
+//const reverseString = (s) => {
+//    for (let start = 0, end = s.length-1; start < end; start++, end--) {
+//        let temp = s[start]
+//        s[start] = s[end]
+//        s[end] = temp
+//    }
+//  };
