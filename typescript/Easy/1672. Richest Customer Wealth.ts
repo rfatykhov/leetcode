325c7bb62ef20af7586da4abff252275ec374da0
Ruslan Fatykhov <rufatykhov@gmail.com>
2022-12-12 12:12:01 +0300

    1672. Richest Customer Wealth

---
+++ typescript/Easy/1672. Richest Customer Wealth.ts
@@ -0,0 +1,10 @@
+//You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+//A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+const maximumWealthTS = (accounts: Array<number[]>) => {
+	const result: Array<number> = [];
+	for (let i = 0; i < accounts.length; i++) {
+		result.push(accounts[i].reduce((a: number, b: number) => a + b));
+	}
+	return Math.max(...result);
+};
